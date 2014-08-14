2bc17d5c2ddaedc69da1dcc708b7ed30c90a266d
Chris Jiang <chrisplus.jiang@gmail.com>
2014-08-14 16:53:01 +0800

    init project

---
+++ MovieUtil.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+
+def splitLinkName(magName):
+	# the mag link example
+	# RV.2006.房车之旅.双语字幕.HR-HDTV.AC3.1024X576.x264.mkv【Auto】
+	pass
+
+
+def fetchFromIMDB(movieName, movieYear):
+	# The IMDB third-party API example
+	# http://www.omdbapi.com/?t=RV&y=2006
+	# t is title
+	# y is year
+
+
+def fetchFromDouban(imdbNo):
+	# The tmp API example
+	# http://api.douban.com/v2/movie/imdb/tt0449089 
+	# tt0449089 is IMDB No.
